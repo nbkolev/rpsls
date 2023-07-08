1fc7745b50b46816410d9f6b0ecf976f96fdd1b7
nbkolev <nbkolev@banchev.bg>
2023-07-08 18:37:01 +0300

    Player Test implemented

---
+++ Rpsls.GameEngine.Tests/PlayerTest.cs
@@ -0,0 +1,40 @@
+using GameEngine;
+using GameEngine;
+namespace Rpsls.GameEngine.Tests;
+
+public class PlayerTest
+{
+    [Fact]
+    void IsExternalPlayer_Abstraction_Working_Correctly()
+    {
+        //Choice id is too high 
+        Assert.Throws<ArgumentException>(() => ExternalPlayerFactory.getFixedChoicePlayerFromRequestArg("666"));
+        //Choice id is negative 
+        Assert.Throws<ArgumentException>(() => ExternalPlayerFactory.getFixedChoicePlayerFromRequestArg("-1"));
+        //Choice id is malformed text
+        Assert.Throws<ArgumentException>(() => ExternalPlayerFactory.getFixedChoicePlayerFromRequestArg("!<dgvm>^@t3248o"));
+
+        //Assert player moves are correct
+        for (int i = 0; i < Enum.GetValues<PlayerMove>().Length; i++)
+        {
+            IPlayer ip = ExternalPlayerFactory.getFixedChoicePlayerFromRequestArg(i.ToString());
+            Assert.True(ip.GetMove().Result == (PlayerMove)i, 
+                "Player move does not correspond to choice id provided.");
+        }
+        
+        
+    }
+    
+    [Fact]
+    void IsBotPlayer_Functioning()
+    {
+        //Assert player moves are correct
+        for (int i = 0; i < Enum.GetValues<PlayerMove>().Length; i++)
+        {
+            IRandSource mockSource = new RandSourceMock(i);
+            IPlayer ip = new BotPlayer(mockSource);
+            Assert.True(ip.GetMove().Result == (PlayerMove)i, 
+                "Player move does not correspond to random choice generated.");
+        }
+    }
+}
