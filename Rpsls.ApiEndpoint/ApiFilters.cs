18989b7f4512ec6e6ff41ab11882fb64702ad5d3
nbkolev <nbkolev@banchev.bg>
2023-07-09 13:09:54 +0300

    ApiExceptionFilter implemented

---
+++ Rpsls.ApiEndpoint/ApiFilters.cs
@@ -0,0 +1,36 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Rpsls.ApiEndpoint;
+
+public class ApiExceptionFilter : IExceptionFilter
+{
+    private readonly IHostEnvironment _hostEnvironment;
+
+    public ApiExceptionFilter(IHostEnvironment hostEnvironment) =>
+        _hostEnvironment = hostEnvironment;
+
+    /// <summary>
+    /// Provide only relevant exception details in production environment.
+    /// </summary>
+    /// 
+    public void OnException(ExceptionContext context)
+    {
+        if (_hostEnvironment.IsDevelopment())
+        {
+            context.Result = new ContentResult
+            {
+                Content = context.Exception.StackTrace
+            };
+          
+        }
+        else
+        {
+            context.Result = new ContentResult
+            {
+                Content = context.Exception.Message
+            };
+        }
+    }
+}
